11c569afc8c26a99ca6fe291ece80f7d22cbfcca
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-03 17:00:04 +0300

    update App.js file

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import './App.css';
+import NavAppTest from "./NavTest/NavAppTest";
+import {BrowserRouter as Router} from 'react-router-dom';
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
-import Task from "./components/Tasks";
-import RequestsDataComponent, {RequestsDataTest} from "./components/RequestsDataComponent";
-import NavApp from "./nav/NavApp";
-import ScrollViewApp from "./scrollview/ScrollViewApp";
-import {AxiosTest} from "./components/AxiosTest";
-import NavAppTest from "./NavTest/NavAppTest";
-import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
-import JsonComponent from "./components/jsondata/JsonComponent";
 
 
 function App() {
 	return (
 		<Router>
 			<div>
-				{/*<HeaderComponent/>*/}
-				{/*<div className="container">*/}
+				<HeaderComponent/>
+				<div className="container">
 					<NavAppTest/>
-
-				{/*</div>*/}
-				{/*<div className="container">*/}
-				{/*	<ScrollViewApp/>*/}
-
-
-				{/*</div>*/}
-				{/*<div>*/}
-				{/*	<RequestsDataComponent/>*/}
-				{/*	<RequestsDataTest/>*/}
-				{/*	<Task/>*/}
-				{/*	<NavApp/>*/}
-				{/*	<AxiosTest/>*/}
-				{/*	<NavAppTest/>*/}
-				{/*	<JsonComponent/>*/}
-				{/*<CustomJsonComponent/>*/}
-				{/*</div>*/}
-				{/*<FooterComponent/>*/}
-
+				</div>
+				<FooterComponent/>
 
 			</div>
 
