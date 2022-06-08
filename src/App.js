229dcef173fd4a581996bc3bf3097ab44e9ef8fd
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-08 17:01:19 +0300

    update App.js file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import React from "react";
 import RouterComponent from "./components/RouterComponent";
-import LayoutComponent from "./components/LayoutComponent";
 
 
 function App() {
 	return (
 			<div>
-				{/*<LayoutComponent>
-					hello
-				</LayoutComponent>*/}
 				<RouterComponent/>
-
 			</div>
 
 
