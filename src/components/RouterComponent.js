0c055af4404451972cb4563901a369ee95a6f1c8
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-08 16:59:53 +0300

    update RouterComponent.js on routing

--- src/components/RouterComponent.js
+++ src/components/RouterComponent.js
@@ -17,10 +17,10 @@ export default function RouterComponent() {
 					<Route path="*" element={<ErrorPage/>}/>
 
 					<Route path="/" element={<LayoutComponent><RequestsLogComponent/></LayoutComponent>}/>
-					<Route path="/atm-logs" element={<RequestsLogComponent/>}/>
-					<Route path="/members-portal-logs" element={<RequestsLogComponent/>}/>
-					<Route path="/my-profile" element={<ProfileComponent/>}/>
-					<Route path="/reset-password" element={<ChangePasswordComponent/>}/>
+					<Route path="/atm-logs" element={<LayoutComponent> <RequestsLogComponent/></LayoutComponent>}/>
+					<Route path="/members-portal-logs" element={<LayoutComponent> <RequestsLogComponent/></LayoutComponent>}/>
+					<Route path="/my-profile" element={<LayoutComponent> <ProfileComponent/></LayoutComponent>}/>
+					<Route path="/reset-password" element={<LayoutComponent> <ChangePasswordComponent/></LayoutComponent>}/>
 
 
 				</Routes>
