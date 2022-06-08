1fd3e613e6ec53dd3b05084c6e14fcb7921f1918
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-08 15:42:20 +0300

    update App.js file

--- src/App.js
+++ src/App.js
@@ -2,19 +2,41 @@ import './App.css';
 import NavigationComponent from "./components/NavigationComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import RequestsLogComponent from "./components/RequestsLogComponent";
+import ProfileComponent from "./components/ProfileComponent";
+import ChangePasswordComponent from "./components/ChangePasswordComponent";
+import ErrorPage from "./components/ErrorPage";
+import LoginComponent from "./components/LoginComponent";
+import React from "react";
 
 
 function App() {
 	return (
+		<Router>
 			<div>
 				<HeaderComponent/>
 
 				<div className="container">
-					<NavigationComponent/>
+					<Routes>
+						<Route path="/login" element={<LoginComponent/>}/>
+						<Route path="*" element={<ErrorPage/>}/>
+
+						<Route path="*" element={<ErrorPage/>}/>
+						<Route path="/" element={<RequestsLogComponent/>}/>
+						<Route path="/atm-logs" element={<RequestsLogComponent/>}/>
+						<Route path="/members-portal-logs" element={<RequestsLogComponent/>}/>
+						<Route path="/my-profile" element={<ProfileComponent/>}/>
+						<Route path="/reset-password" element={<ChangePasswordComponent/>}/>
+
+
+					</Routes>
 				</div>
 				<FooterComponent/>
 
 			</div>
+		</Router>
+
 
 	);
 }
