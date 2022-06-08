30266cbb57916543cfbe98049de10b9aa69652f8
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-08 16:12:15 +0300

    update App.js file

---
+++ src/components/LayoutComponent.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import 'devextreme/dist/css/dx.light.css';
+import './styles/NavigationStyles.css';
+import FooterComponent from "./FooterComponent";
+import HeaderComponent from "./HeaderComponent";
+
+export default class NavigationComponent extends React.Component {
+
+	render() {
+
+
+		return (
+			<div>
+				<HeaderComponent/>
+				<div className="container">
+					<div className="left-content">
+						{/*<div>*/}
+						{/*	<Link to="dashboard/">ATM Bridge Logs</Link>*/}
+						{/*</div>*/}
+						{/*<div>*/}
+						{/*	<Link to="dashboard/atm-logs">ATM Bridge Logs</Link>*/}
+						{/*</div>*/}
+						{/*<div>*/}
+						{/*	<Link to="dashboard/members-portal-logs">Members Portal Logs </Link>*/}
+						{/*</div>*/}
+						{/*<div>*/}
+						{/*	<Link to="dashboard/my-profile">My Profile</Link>*/}
+						{/*</div>*/}
+						{/*<div>*/}
+						{/*	<Link to="dashboard/reset-password">Reset Password</Link>*/}
+						{/*</div>*/}
+						{/*<div>*/}
+						{/*	<Link to="/login">Login page</Link>*/}
+						{/*</div>*/}
+					</div>
+					<div className="right-content">
+
+
+					</div>
+				</div>
+				<FooterComponent/>
+			</div>
+
+		);
+	}
+
+
+}
+
+
+
