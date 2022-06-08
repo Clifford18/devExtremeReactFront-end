e6cce32145a51645f4a3f8cc8fac02eb96eb0d6b
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-08 09:02:30 +0300

    update App.js file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import './App.css';
 import NavigationComponent from "./components/NavigationComponent";
-import {BrowserRouter as Router} from 'react-router-dom';
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
-import ScrollViewApp from "./scrollview/ScrollViewApp";
 
 
 function App() {
 	return (
-		<Router>
 			<div>
 				<HeaderComponent/>
 
 				<div className="container">
 					<NavigationComponent/>
-					{/*<ScrollViewApp/>*/}
 				</div>
 				<FooterComponent/>
 
 			</div>
-
-		</Router>
 
 	);
 }
