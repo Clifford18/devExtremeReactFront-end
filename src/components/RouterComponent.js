23ccabb9189ed05dac5147f77546930e481aa7eb
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-09 10:29:13 +0300

    update RouterComponent.js file on formats

--- src/components/RouterComponent.js
+++ src/components/RouterComponent.js
@@ -15,18 +15,13 @@ export default function RouterComponent() {
 				<Routes>
 					<Route path="/login" element={<LoginComponent/>}/>
 					<Route path="*" element={<ErrorPage/>}/>
-
 					<Route path="/" element={<LayoutComponent><RequestsLogComponent/></LayoutComponent>}/>
 					<Route path="/atm-logs" element={<LayoutComponent> <RequestsLogComponent/></LayoutComponent>}/>
 					<Route path="/members-portal-logs" element={<LayoutComponent> <RequestsLogComponent/></LayoutComponent>}/>
 					<Route path="/my-profile" element={<LayoutComponent> <ProfileComponent/></LayoutComponent>}/>
 					<Route path="/reset-password" element={<LayoutComponent> <ChangePasswordComponent/></LayoutComponent>}/>
-
-
 				</Routes>
 			</div>
 		</Router>
-
-
 	);
 };
