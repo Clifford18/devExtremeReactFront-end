712713728c31d25ee0d09515304eba6e6bf69700
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-09 10:28:30 +0300

    update LayoutComponent.js file with links to pages

--- src/components/LayoutComponent.js
+++ src/components/LayoutComponent.js
@@ -3,6 +3,7 @@ import 'devextreme/dist/css/dx.light.css';
 import './styles/NavigationStyles.css';
 import FooterComponent from "./FooterComponent";
 import HeaderComponent from "./HeaderComponent";
+import {Link} from "react-router-dom";
 
  const LayoutComponent = (props) => {
 
@@ -12,24 +13,21 @@ import HeaderComponent from "./HeaderComponent";
 			<HeaderComponent/>
 			<div className="container">
 				<div className="left-content">
-					{/*<div>*/}
-					{/*	<Link to="dashboard/">ATM Bridge Logs</Link>*/}
-					{/*</div>*/}
-					{/*<div>*/}
-					{/*	<Link to="dashboard/atm-logs">ATM Bridge Logs</Link>*/}
-					{/*</div>*/}
-					{/*<div>*/}
-					{/*	<Link to="dashboard/members-portal-logs">Members Portal Logs </Link>*/}
-					{/*</div>*/}
-					{/*<div>*/}
-					{/*	<Link to="dashboard/my-profile">My Profile</Link>*/}
-					{/*</div>*/}
-					{/*<div>*/}
-					{/*	<Link to="dashboard/reset-password">Reset Password</Link>*/}
-					{/*</div>*/}
-					{/*<div>*/}
-					{/*	<Link to="/login">Login page</Link>*/}
-					{/*</div>*/}
+					<div>
+						<Link to="/atm-logs">ATM Bridge Logs</Link>
+					</div>
+					<div>
+						<Link to="/members-portal-logs">Members Portal Logs </Link>
+					</div>
+					<div>
+						<Link to="/my-profile">My Profile</Link>
+					</div>
+					<div>
+						<Link to="/reset-password">Reset Password</Link>
+					</div>
+					<div>
+						<Link to="/login">Login page</Link>
+					</div>
 				</div>
 				<div className="right-content" >
 					{props.children}
@@ -38,10 +36,7 @@ import HeaderComponent from "./HeaderComponent";
 			</div>
 			<FooterComponent/>
 		</div>
-
 	);
-
-
 };
 
  export default LayoutComponent;
