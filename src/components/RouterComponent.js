c5f4f219600d2219cebcd932b3bb95f98702c7a7
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-20 15:05:29 +0300

    update RouterComponent.js file

--- src/components/RouterComponent.js
+++ src/components/RouterComponent.js
@@ -1,4 +1,3 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import React from "react";
 import LoginComponent from "./LoginComponent";
 import ErrorPage from "./ErrorPage";
@@ -7,6 +6,9 @@ import ProfileComponent from "./ProfileComponent";
 import ChangePasswordComponent from "./ChangePasswordComponent";
 import LayoutComponent from "./LayoutComponent";
 
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import RequireAuth from "./RequireAuth";
+
 
 export default function RouterComponent() {
 	return (
@@ -14,12 +16,18 @@ export default function RouterComponent() {
 			<div>
 				<Routes>
 					<Route path="/login" element={<LoginComponent/>}/>
+
+
+					{/*Protected routes*/}
+					<Route path="/" element={<RequireAuth> <LayoutComponent><RequestsLogComponent/></LayoutComponent></RequireAuth>}/>
+					<Route path="/atm-logs" element={<RequireAuth> <LayoutComponent> <RequestsLogComponent/></LayoutComponent></RequireAuth>}/>
+					<Route path="/members-portal-logs" element={<RequireAuth> <LayoutComponent> <RequestsLogComponent/></LayoutComponent></RequireAuth>}/>
+					<Route path="/my-profile" element={<RequireAuth> <LayoutComponent> <ProfileComponent/></LayoutComponent></RequireAuth>}/>
+					<Route path="/reset-password" element={<RequireAuth> <LayoutComponent> <ChangePasswordComponent/></LayoutComponent></RequireAuth>}/>
+
+					{/*Catch all Path*/}
 					<Route path="*" element={<ErrorPage/>}/>
-					<Route path="/" element={<LayoutComponent><RequestsLogComponent/></LayoutComponent>}/>
-					<Route path="/atm-logs" element={<LayoutComponent> <RequestsLogComponent/></LayoutComponent>}/>
-					<Route path="/members-portal-logs" element={<LayoutComponent> <RequestsLogComponent/></LayoutComponent>}/>
-					<Route path="/my-profile" element={<LayoutComponent> <ProfileComponent/></LayoutComponent>}/>
-					<Route path="/reset-password" element={<LayoutComponent> <ChangePasswordComponent/></LayoutComponent>}/>
+
 				</Routes>
 			</div>
 		</Router>
