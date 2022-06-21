4ccf73f4ef13f8b85dffc61bc1d6f0229920302c
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-21 11:49:04 +0300

    update RouterComponent.js file

--- src/components/RouterComponent.js
+++ src/components/RouterComponent.js
@@ -17,13 +17,19 @@ export default function RouterComponent() {
 				<Routes>
 					<Route path="/login" element={<LoginComponent/>}/>
 
-
 					{/*Protected routes*/}
-					<Route path="/" element={<RequireAuth> <LayoutComponent><RequestsLogComponent/></LayoutComponent></RequireAuth>}/>
-					<Route path="/atm-logs" element={<RequireAuth> <LayoutComponent> <RequestsLogComponent/></LayoutComponent></RequireAuth>}/>
-					<Route path="/members-portal-logs" element={<RequireAuth> <LayoutComponent> <RequestsLogComponent/></LayoutComponent></RequireAuth>}/>
-					<Route path="/my-profile" element={<RequireAuth> <LayoutComponent> <ProfileComponent/></LayoutComponent></RequireAuth>}/>
-					<Route path="/reset-password" element={<RequireAuth> <LayoutComponent> <ChangePasswordComponent/></LayoutComponent></RequireAuth>}/>
+					<Route path="/" element={<LayoutComponent><RequestsLogComponent/></LayoutComponent>}/>
+					<Route path="/atm-logs" element={<LayoutComponent> <RequestsLogComponent/></LayoutComponent>}/>
+					<Route path="/members-portal-logs" element={<LayoutComponent> <RequestsLogComponent/></LayoutComponent>}/>
+					<Route path="/my-profile" element={<LayoutComponent> <ProfileComponent/></LayoutComponent>}/>
+					<Route path="/reset-password" element={<LayoutComponent> <ChangePasswordComponent/></LayoutComponent>}/>
+
+					{/*/!*Protected routes*!/*/}
+					{/*<Route path="/" element={<RequireAuth> <LayoutComponent><RequestsLogComponent/></LayoutComponent></RequireAuth>}/>*/}
+					{/*<Route path="/atm-logs" element={<RequireAuth> <LayoutComponent> <RequestsLogComponent/></LayoutComponent></RequireAuth>}/>*/}
+					{/*<Route path="/members-portal-logs" element={<RequireAuth> <LayoutComponent> <RequestsLogComponent/></LayoutComponent></RequireAuth>}/>*/}
+					{/*<Route path="/my-profile" element={<RequireAuth> <LayoutComponent> <ProfileComponent/></LayoutComponent></RequireAuth>}/>*/}
+					{/*<Route path="/reset-password" element={<RequireAuth> <LayoutComponent> <ChangePasswordComponent/></LayoutComponent></RequireAuth>}/>*/}
 
 					{/*Catch all Path*/}
 					<Route path="*" element={<ErrorPage/>}/>
